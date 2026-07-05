9b7d3cd5e81d7e8785f2ec820031b3644701e515
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-07-05 22:06:13 +0500

    player: rotate Camera by x axis

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+@export var mouseSensetivity = 0.001
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x*0.005)
+		rotate_y(-event.relative.x*mouseSensetivity)
+		$Camera.rotate_x(event.relative.y*mouseSensetivity)
