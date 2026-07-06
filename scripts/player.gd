33ee0598070c3f176549f63b197508d459711836
GazizVR <gazizvalitov230378@gmail.com>
2026-07-06 16:37:13 +0500

    player: гравитация

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,19 +12,26 @@ func _input(event: InputEvent) -> void:
 		)
 		
 @export var speed = 8
+@export var gravity = 50
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
-	if Input.is_action_pressed("walk_forward"):
-		direction += global_transform.basis.z
-	if Input.is_action_pressed("walk_back"):
-		direction -= global_transform.basis.z
-	if Input.is_action_pressed("walk_right"):
-		direction -= global_transform.basis.x
-	if Input.is_action_pressed("walk_left"):
-		direction += global_transform.basis.x
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	if not is_on_floor():
+		direction.y -= gravity * delta
+		target_velocity.y = direction.y
+	else:
+		if Input.is_action_pressed("walk_forward"):
+			direction += global_transform.basis.z
+		if Input.is_action_pressed("walk_back"):
+			direction -= global_transform.basis.z
+		if Input.is_action_pressed("walk_right"):
+			direction -= global_transform.basis.x
+		if Input.is_action_pressed("walk_left"):
+			direction += global_transform.basis.x
+		if direction != Vector3.ZERO:
+			direction = direction.normalized()
+		target_velocity.x = direction.x * speed
+		target_velocity.z = direction.z * speed
 	velocity = target_velocity
 	move_and_slide()
