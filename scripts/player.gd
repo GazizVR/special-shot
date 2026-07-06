fe452a40966b7de4aa138aceb594e61192bf71f0
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-07-06 12:42:20 +0500

    player: ограничение по повроту камеры по оси x

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,4 +5,9 @@ extends CharacterBody3D
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x*mouseSensetivity)
-		$Camera.rotate_x(event.relative.y*mouseSensetivity)
+		$Camera.rotation.x += event.relative.y*mouseSensetivity
+		$Camera.rotation.x = clamp(
+			$Camera.rotation.x,
+			deg_to_rad(-60),
+			deg_to_rad(60)
+		)
