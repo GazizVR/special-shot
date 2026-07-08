b2a0903270376e4509426c5c0c1312f8bd91e890
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-07-08 12:09:02 +0500

    player: crouch features

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,17 +9,19 @@ func _input(event: InputEvent) -> void:
 		$Hand.rotation.x = clamp(
 			$Hand.rotation.x+vertical_rotaion,
 			deg_to_rad(-60),
-			deg_to_rad(60)
+			deg_to_rad(90)
 		)
 		$Camera.rotation.x = clamp(
 			$Camera.rotation.x-vertical_rotaion,
 			deg_to_rad(-60),
-			deg_to_rad(60)
+			deg_to_rad(90)
 		)
 		
 @export var speed = 8
 @export var gravity = 100
 var target_velocity = Vector3.ZERO
+var is_crouch = false
+@export var crouch_per = 0.6
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
@@ -30,6 +32,20 @@ func _physics_process(delta: float) -> void:
 		direction.y -= gravity * delta
 		target_velocity.y = direction.y
 	else:
+		if Input.is_action_just_pressed("crouch"):
+			is_crouch = !is_crouch
+			if is_crouch:
+				$Body.scale.y *= crouch_per
+				$Model.scale.y *= crouch_per
+				$Camera.global_position.y *= crouch_per
+				$Hand.global_position.y *= crouch_per
+				speed *= crouch_per
+			else:
+				$Body.scale.y /= crouch_per
+				$Model.scale.y /= crouch_per
+				$Camera.global_position.y /= crouch_per
+				$Hand.global_position.y /= crouch_per
+				speed /= crouch_per
 		if Input.is_action_pressed("walk_forward"):
 			direction += global_transform.basis.z
 		if Input.is_action_pressed("walk_back"):
