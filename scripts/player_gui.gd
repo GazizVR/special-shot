d78b6680cd2215869ec95f6623fa6d6ea7f2c444
GazizVR <gazizvalitov230378@gmail.com>
2026-07-11 08:10:18 +0500

    fix(player): расположение HpLabel строго слева сверху и Window Stretch = true,expand

--- scripts/player_gui.gd
+++ scripts/player_gui.gd
@@ -2,7 +2,8 @@ extends Control
 
 func _draw() -> void:
 	if !is_multiplayer_authority(): return
-	draw_circle(Vector2(0,0),2.0,Color.GREEN)	
+	var win_size = get_viewport().get_visible_rect().size/2
+	draw_circle(win_size,2.0,Color.GREEN)	
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PAUSED:
